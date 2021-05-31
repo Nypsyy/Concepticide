d4013d12dbf850180a3e1305dfa27a36e7422cdf
Camille Benoit <camille.benoit@mines-ales.org>
2021-05-31 19:41:37 -0400

    oui

--- Concepticide/Assets/Scripts/Dealer.cs
+++ Concepticide/Assets/Scripts/Dealer.cs
@@ -8,11 +8,30 @@ public class Dealer : MonoBehaviour
     public Dialogue dialogue1;
     private Dialogue dialogue2;
 
+    void Start()
+    {
+
+    }
+
     // Update is called once per frame
     void OnMouseDown()
     {
         Debug.Log("j'ai cliqué sur " + this.gameObject.name);
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue1);
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+
+            Debug.Log("j'ai cliqué sur ");
+        }
+
+        if(Input.GetKeyDown("h"))
+        {
+            Debug.Log("oui");
+        }
     }
     
 }
